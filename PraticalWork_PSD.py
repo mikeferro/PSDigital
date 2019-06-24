b5fb5366a6f64971b08cda6f9900047e14ba4e54
michellferro <mike.ferro87@gmail.com>
2019-06-24 16:33:26 +0100

    gravar som

--- PraticalWork_PSD.py
+++ PraticalWork_PSD.py
@@ -11,3 +11,17 @@ import pyaudio
 import numpy as np
 import wave
 import datetime
+
+# initialise pyaudio
+p = pyaudio.PyAudio()
+
+# open stream
+buffer_size = 4096
+pyaudio_format = pyaudio.paInt16
+n_channels = 1
+samplerate = 48000
+record_sec = 5
+now_ts = datetime.datetime.now()
+now_ts_str = now_ts.strftime("%Y-%m-%d_%H-%M-%S")
+print('Current Timestamp : ', now_ts_str)
+WAVE_OUTPUT_FILENAME =  now_ts_str + ".wav"
